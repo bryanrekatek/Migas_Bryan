451332019c265577f3d8c4fb18bfa43077ef3ddb
Budi <bryanrekatek@gmail.com>
2023-06-20 07:27:36 +0700

    add : register & login flow

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -37,21 +37,34 @@
 <div class="login-area">
     <div class="container">
         <div class="login-box ptb--100">
-            <form method="POST" action="{{ route('login') }}">
+            <form method="POST" action="{{ route('register') }}">
                 @csrf
                 <div class="login-form-head">
-                    <h4>Sign In</h4>
-                    <p>Hello there, Sign in and start managing your Admin Template</p>
+                    <h4>Sign up</h4>
+                    <p>Hello there, Sign up and Join with Us</p>
                 </div>
                 <div class="login-form-body">
                     <div class="form-gp">
+                        <label for="name">Full Name</label>
+{{--                        <input type="text" id="exampleInputName1">--}}
+                        <input id="name" type="text"  @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name">
+                        @error('name')
+                        <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                        @enderror
+                        <i class="ti-user"></i>
+                        <div class="text-danger"></div>
+                    </div>
+                    <div class="form-gp">
                         <label for="email">Email address</label>
 {{--                        <input type="email" id="exampleInputEmail1">--}}
-                        <input id="email" type="email" @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                        <input id="email" type="email" @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+
                         @error('email')
                         <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
+                                        <strong>{{ $message }}</strong>
+                                    </span>
                         @enderror
                         <i class="ti-email"></i>
                         <div class="text-danger"></div>
@@ -59,41 +72,38 @@
                     <div class="form-gp">
                         <label for="password">Password</label>
 {{--                        <input type="password" id="exampleInputPassword1">--}}
-                        <input id="password" type="password" @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
+                        <input id="password" type="password" @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
                         @error('password')
                         <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
+                                        <strong>{{ $message }}</strong>
+                                    </span>
                         @enderror
                         <i class="ti-lock"></i>
                         <div class="text-danger"></div>
                     </div>
-                    <div class="row mb-4 rmber-area">
-                        <div class="col-6">
-                            <div class="custom-control custom-checkbox mr-sm-2">
-{{--                                <input type="checkbox" class="custom-control-input" id="customControlAutosizing">--}}
-                                <input class="custom-control-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-                                <label class="custom-control-label" for="customControlAutosizing">Remember Me</label>
-                            </div>
-                        </div>
-                        <div class="col-6 text-right">
-                            <a href="#">Forgot Password?</a>
-                        </div>
+                    <div class="form-gp">
+                        <label for="password-confirm">Confirm Password</label>
+{{--                        <input type="password" id="exampleInputPassword2">--}}
+                        <input id="password-confirm" type="password" name="password_confirmation" required autocomplete="new-password">
+                        <i class="ti-lock"></i>
+                        <div class="text-danger"></div>
                     </div>
                     <div class="submit-btn-area">
-                        <button id="form_submit" type="submit">Submit <i class="ti-arrow-right"></i></button>
+{{--                        <button id="form_submit" type="submit">Submit <i class="ti-arrow-right"></i></button>--}}
+                        <button type="submit" class="btn btn-primary">
+                            {{ __('Register') }}
+                        </button>
 {{--                        <div class="login-other row mt-4">--}}
 {{--                            <div class="col-6">--}}
-{{--                                <a class="fb-login" href="#">Log in with <i class="fa fa-facebook"></i></a>--}}
+{{--                                <a class="fb-login" href="#">Sign up with <i class="fa fa-facebook"></i></a>--}}
 {{--                            </div>--}}
 {{--                            <div class="col-6">--}}
-{{--                                <a class="google-login" href="#">Log in with <i class="fa fa-google"></i></a>--}}
+{{--                                <a class="google-login" href="#">Sign up with <i class="fa fa-google"></i></a>--}}
 {{--                            </div>--}}
 {{--                        </div>--}}
                     </div>
                     <div class="form-footer text-center mt-5">
-                        <p class="text-muted">Don't have an account? <a href="{{ route('reg') }}">Sign up</a></p>
+                        <p class="text-muted">Don't have an account? <a href="{{ route('signin') }}">Sign in</a></p>
                     </div>
                 </div>
             </form>
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('signin');
+Route::get('/reg', function () {
+    return view('register');
+})->name('reg');
 
 Auth::routes();
 
